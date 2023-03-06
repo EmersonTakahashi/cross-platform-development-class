efbd2be69cdc198a65eb30a9a9a5474b7afa7700
Emerson Takahashi <emer_tak@hotmail.com>
2023-03-06 20:07:25 +0000

    exercise 4, calculating age

--- Aula1/script.js
+++ Aula1/script.js
@@ -25,23 +25,60 @@
 // console.log(myArray.sort().at(0));
 
 //exercise 3
-const car = {
-  brand: "Renault",
-  model: "Megane",
-  run: function (secondsRunningInMs) {
-    const brand = this.brand;
-    const model = this.model;
-    let count = 0;
+// const car = {
+//   brand: "Renault",
+//   model: "Megane",
+//   run: function (secondsRunningInMs) {
+//     const brand = this.brand;
+//     const model = this.model;
+//     let count = 0;
 
-    setInterval(function () {
-      let running = "";
-      for (let i = 0; i < count; i++) {
-        running = running.concat(".");
+//     setInterval(function () {
+//       let running = "";
+//       for (let i = 0; i < count; i++) {
+//         running = running.concat(".");
+//       }
+//       const car = `${brand} : ${model}`;
+//       console.clear();
+//       console.log(running.concat(car));
+//       count++;
+//     }, secondsRunningInMs);
+//   },
+// };
+// car.run(2000);
+
+//exercise 4
+const person = {
+  firstName: "Emerson",
+  lastName: "Takahashi",
+  placeOfBirth: "Itapecerica da Serra",
+  birthDate: new Date(2000, 4, 23),
+  hobbies: ["Watch movies", "Counter Strike", "Chess"],
+  calculateAge: function () {
+    // return Math.floor(
+    //   (Date.now() - this.birthDate) / (1000 * 60 * 60 * 24 * 365.25)
+    // );
+
+    //hard way
+    const currentDate = new Date();
+    if (currentDate.getMonth() >= this.birthDate.getMonth()) {
+      if (currentDate.getDay() >= this.birthDate.getDay()) {
+        return currentDate.getFullYear() - this.birthDate.getFullYear();
+      } else {
+        return currentDate.getFullYear() - this.birthDate.getFullYear() - 1;
       }
-      const car = `${brand} : ${model}`;
-      console.log(running.concat(car));
-      count++;
-    }, secondsRunningInMs);
+    } else {
+      return currentDate.getFullYear() - this.birthDate.getFullYear() - 1;
+    }
   },
 };
-car.run(2000);
+
+console.log(
+  `${person.firstName} ${
+    person.lastName
+  } was born in ${person.birthDate.toDateString()} at ${
+    person.placeOfBirth
+  } (${person.calculateAge()} years old), nowadays has these hobbies: ${
+    person.hobbies
+  }`
+);
