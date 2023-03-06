363b0ba00191cab43dd78a86a54bcf6b72136cc5
Emerson Takahashi <emer_tak@hotmail.com>
2023-03-06 20:22:25 +0000

    WIP started regex exercise and fixing exercise 3 to get seconds instead of ms

--- Aula1/script.js
+++ Aula1/script.js
@@ -28,7 +28,7 @@
 // const car = {
 //   brand: "Renault",
 //   model: "Megane",
-//   run: function (secondsRunningInMs) {
+//   run: function (secondsRunning) {
 //     const brand = this.brand;
 //     const model = this.model;
 //     let count = 0;
@@ -42,43 +42,48 @@
 //       console.clear();
 //       console.log(running.concat(car));
 //       count++;
-//     }, secondsRunningInMs);
+//     }, secondsRunning * 1000);
 //   },
 // };
-// car.run(2000);
+// car.run(2);
 
 //exercise 4
-const person = {
-  firstName: "Emerson",
-  lastName: "Takahashi",
-  placeOfBirth: "Itapecerica da Serra",
-  birthDate: new Date(2000, 4, 23),
-  hobbies: ["Watch movies", "Counter Strike", "Chess"],
-  calculateAge: function () {
-    // return Math.floor(
-    //   (Date.now() - this.birthDate) / (1000 * 60 * 60 * 24 * 365.25)
-    // );
+// const person = {
+//   firstName: "Emerson",
+//   lastName: "Takahashi",
+//   placeOfBirth: "Itapecerica da Serra",
+//   birthDate: new Date(2000, 4, 23),
+//   hobbies: ["Watch movies", "Counter Strike", "Chess"],
+//   calculateAge: function () {
+//     // return Math.floor(
+//     //   (Date.now() - this.birthDate) / (1000 * 60 * 60 * 24 * 365.25)
+//     // );
 
-    //hard way
-    const currentDate = new Date();
-    if (currentDate.getMonth() >= this.birthDate.getMonth()) {
-      if (currentDate.getDay() >= this.birthDate.getDay()) {
-        return currentDate.getFullYear() - this.birthDate.getFullYear();
-      } else {
-        return currentDate.getFullYear() - this.birthDate.getFullYear() - 1;
-      }
-    } else {
-      return currentDate.getFullYear() - this.birthDate.getFullYear() - 1;
-    }
-  },
-};
+//     //hard way
+//     const currentDate = new Date();
+//     if (currentDate.getMonth() >= this.birthDate.getMonth()) {
+//       if (currentDate.getDay() >= this.birthDate.getDay()) {
+//         return currentDate.getFullYear() - this.birthDate.getFullYear();
+//       } else {
+//         return currentDate.getFullYear() - this.birthDate.getFullYear() - 1;
+//       }
+//     } else {
+//       return currentDate.getFullYear() - this.birthDate.getFullYear() - 1;
+//     }
+//   },
+// };
 
-console.log(
-  `${person.firstName} ${
-    person.lastName
-  } was born in ${person.birthDate.toDateString()} at ${
-    person.placeOfBirth
-  } (${person.calculateAge()} years old), nowadays has these hobbies: ${
-    person.hobbies
-  }`
-);
+// console.log(
+//   `${person.firstName} ${
+//     person.lastName
+//   } was born in ${person.birthDate.toDateString()} at ${
+//     person.placeOfBirth
+//   } (${person.calculateAge()} years old), nowadays has these hobbies: ${
+//     person.hobbies
+//   }`
+// );
+
+//exercise 6
+const input = "+351123456789";
+const phoneRegex = "";
+console.log(phoneRegex.test());
