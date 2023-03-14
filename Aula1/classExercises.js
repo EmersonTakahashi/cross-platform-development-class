edf12eb166b7ffeb06952f9ee3ed3b15f4924a94
Emerson Takahashi <emer_tak@hotmail.com>
2023-03-14 19:21:51 +0000

    adding some validations at set numbers of ports

--- Aula1/classExercises.js
+++ Aula1/classExercises.js
@@ -10,10 +10,8 @@ class Carro extends Vehicle {
   #temAC = false;
   static #quatroPortasCount = 0;
 
-  constructor(marca, modelo, numeroDePortas = 2, temAC = false) {
+  constructor(marca, modelo) {
     super(marca, modelo);
-    this.#setNumeroDePortas = numeroDePortas;
-    this.#temAC = temAC;
   }
 
   static get getQuatroPortasCount() {
@@ -32,24 +30,34 @@ class Carro extends Vehicle {
     return this.#temAC;
   }
 
-  set #setNumeroDePortas(numeroDePortas) {
-    if (numeroDePortas === 4) {
+  set setNumeroDePortas(numeroDePortas) {
+    if (this.numeroDePortas !== 4 && numeroDePortas === 4) {
       Carro.#increaseQuatroPortasQuantity(1);
+    }
+
+    if (this.numeroDePortas === 4 && numeroDePortas !== 4) {
+      Carro.#increaseQuatroPortasQuantity(-1);
     }
 
     this.#numeroDePortas = numeroDePortas;
   }
 
-  set #setTemAC(temAC) {
+  set setTemAC(temAC) {
     this.#temAC = temAC;
   }
 }
 
-const carro1 = new Carro("Ford", "Golf", 2, true);
-const carro2 = new Carro("Ford", "Golf", 4, true);
-const carro3 = new Carro("Ford", "Golf", 4, false);
-const carro4 = new Carro("Ford", "Golf", 4, false);
-const carro5 = new Carro("Ford", "Golf", 2, true);
+const carro1 = new Carro("Ford", "Golf");
+const carro2 = new Carro("Ford", "Golf");
+const carro3 = new Carro("Ford", "Golf");
+const carro4 = new Carro("Ford", "Golf");
+const carro5 = new Carro("Ford", "Golf");
+carro1.setNumeroDePortas = 2;
+carro2.setNumeroDePortas = 4;
+carro3.setNumeroDePortas = 2;
+carro4.setNumeroDePortas = 4;
+carro5.setNumeroDePortas = 2;
+
 console.log(carro1, carro2, carro3, carro4, carro5);
 
 console.log(Carro.getQuatroPortasCount);
